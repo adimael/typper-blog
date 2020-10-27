81d81d0c0a4b4c169c9349559e2467be075247cc
fincorpcrm <fincorpcrm@gmail.com>
2020-10-26 21:17:38 -0300

    Modificações de design typper-blog

--- suporte/blog.php
+++ suporte/blog.php
@@ -548,7 +548,7 @@ if($_SESSION['access_level'] != 'Suporte'){
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Cadastrar novo produto</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Nova postagem</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -592,7 +592,7 @@ if($_SESSION['access_level'] != 'Suporte'){
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Cadastrar novo produto</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Editar postagem</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
