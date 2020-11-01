fa3d899908db0cd4298dc259809583b37dc0f6f9
fincorpcrm <fincorpcrm@gmail.com>
2020-11-01 14:24:24 -0300

    tag meta adicionada typper-blog

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@ require("suporte/function/limita-texto.php");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="var(--color-secondary);">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Typper</title>
   <link rel="icon" href="favicon.ico" type="/favicon.ico" />
